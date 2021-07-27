4929f25778cfddb824dfcb81632db163c18e373f
doda <ddark.kr@gmail.com>
2021-07-27 20:41:36 +0900

    fix: toss 어플리케이션이 없을 때 오류 나는 문제

--- app/src/main/java/dev/doda/easyQrCheckIn/SchemeHelper.kt
+++ app/src/main/java/dev/doda/easyQrCheckIn/SchemeHelper.kt
@@ -1,13 +1,22 @@
 package dev.doda.easyQrCheckIn
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
+import android.view.Gravity
+import android.widget.Toast
+import kotlin.system.exitProcess
 
 class SchemeHelper {
     fun startTossQrCheckIn(activity: Activity) {
-        val url = "supertoss://qr-checkin"
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        activity.startActivity(intent)
+        try {
+            val url = "supertoss://qr-checkin"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            activity.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            val toast = Toast.makeText(activity.applicationContext, "Toss 앱이 설치되어 있는지 확인해주세요.", Toast.LENGTH_LONG)
+            toast.show()
+        }
     }
 }
